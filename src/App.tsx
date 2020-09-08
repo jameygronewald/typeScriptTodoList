a6bc78b22074a1e29b59e8a6963618b39e726b82
Jamey Gronewald <jrgronewald@gmail.com>
2020-09-07 17:10:38 -0700

    removed unnecessary boilerplate; added todo form that is submitting typed Todo objects based on Todo interface and adding to Todo[]; mapping through todos on useState hook and displaying on the DOM; updates on submit with new todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+type FormElem = React.FormEvent<HTMLFormElement>;
+
+interface Todo {
+  text: string;
+  isComplete: boolean;
+}
+
+function App(): JSX.Element {
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const newTodo: Todo[] = [...todos, { text, isComplete: false }];
+    setTodos(newTodo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        <button type="submit">Add a Todo</button>
+      </form>
+      <section>
+        {todos.map((todo: Todo, index: number) => (
+          <div key={index}>{todo.text}</div>
+        ))}
+      </section>
+    </>
   );
 }
 
